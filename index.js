5dffb730f2f06b61e1fc61c5158930ce68fd3c9d
Daria Imbireva <imbireva.daria@gmail.com>
2022-12-09 18:02:11 +0300

    feat: add script to open & close menu

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const nav = document.querySelector('.nav-list');
+const hamburger = document.querySelector('.hamburger');
+
+const addClassOpen = () => {
+  page.classList.add('page_nav-open');
+  nav.classList.add('nav-list_nav-open');
+  hamburger.classList.add('hamburger_nav-open');
+};
+
+const removeClassOpen = () => {
+  page.classList.remove('page_nav-open');
+  nav.classList.remove('nav-list_nav-open');
+  hamburger.classList.remove('hamburger_nav-open');
+};
+
+hamburger.addEventListener('click', () => {
+  if (hamburger.classList.contains('hamburger_nav-open')) removeClassOpen();
+  else addClassOpen();
+});
+
+nav.addEventListener('click', (event) => {
+  if (event.target.classList.contains('link')) removeClassOpen();
+});
